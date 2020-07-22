529f6ab9020cabc9f02a534ef8a0740c97d3d012
Carolina Chico <chico.carolina@gmail.com>
2020-07-22 18:24:06 -0300

    Modulo 001 - Ejercico 3.4 Estructuras basicas Instanciacion

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/StudentDetails.java
@@ -0,0 +1,11 @@
+package com.globallogic.app;
+
+public class StudentDetails {
+	public void studentAge(int age, int b) {
+		int c;
+		c = age + b;
+		System.out.println("La edad del estudiante es: " + c);
+		
+	}
+
+}
---
+++ bootcampgl2020/src/main/java/com/globallogic/app/VariableLocal.java
@@ -0,0 +1,16 @@
+package com.globallogic.app;
+
+public class VariableLocal {
+	public static void main(String[] args) {
+		int age = 0;
+		int b = 5;
+		
+		StudentDetails estudiante = new StudentDetails();
+		estudiante.studentAge(age, b);
+		
+		
+	}
+
+	
+
+}
