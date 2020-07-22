ed619d19463ebc2c249173b44a4e79591d84ef71
Carolina Chico <chico.carolina@gmail.com>
2020-07-22 09:37:14 -0300

    Modulo 001 - Ejercicio 3.1 Estructuras basicas

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/TiposPrimitivos.java
@@ -0,0 +1,17 @@
+package com.globallogic.app;
+
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+		int myNum = 15;
+		char A = 'a';
+		boolean b;
+		b=true;
+		float f1= (float)2436.2456;
+		short c= (short)500;
+		System.out.println("El valor de mi numero es " + myNum);
+		System.out.println("El valor del caracter es " + A);
+		System.out.println("El valor de b es " + b);
+		System.out.println("El valor de mifloat es " + f1);
+		System.out.println("El valor de mishort es " + c);
+	}
+}
