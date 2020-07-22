a35a74ec7d8840d8288cb00aea8bdfc6b9a08e7e
Carolina Chico <chico.carolina@gmail.com>
2020-07-22 17:30:12 -0300

    Modulo 001 - Ejercicio 3.3 Estructuras basicas List

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/PrimeraLista.java
@@ -0,0 +1,18 @@
+package com.globallogic.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeraLista {
+	public static void main(String[] args) {
+		List<String> letters = new ArrayList<String>();
+		letters.add("A");
+		letters.add("B");
+		letters.add("C");
+		System.out.println(letters);
+		System.out.println("El numero de elementos de la lista es " + letters.size());
+		letters.add(0,"X");
+		System.out.println(letters);
+		System.out.println("El numero de elementos de la lista es " + letters.size());
+	}
+}
