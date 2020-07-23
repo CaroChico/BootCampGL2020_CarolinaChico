1a02d7816dfdc46c90b2c0b498522dc2f446f893
Carolina Chico <chico.carolina@gmail.com>
2020-07-23 15:10:54 -0300

    Modulo 001 - Ejercicio 4.1 control de Flujos IF

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/ContolFlujoIf.java
@@ -0,0 +1,20 @@
+package com.globallogic.app;
+
+public class ContolFlujoIf {
+
+	public static void main(String[] args) {
+		
+		int a = 3;
+		int b = 10;
+		int c = a + b;
+		
+		if (c <= 10) {
+		
+			System.out.println("La suma de los numeros es: " + c);
+		} else {
+				System.out.println("La suma de los numeros es mayor a 10");
+			
+			
+		}	
+	}
+}
