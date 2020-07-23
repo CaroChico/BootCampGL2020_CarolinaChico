82eb9fbe0ec0385332db4b8a76097bf703086ca8
Carolina Chico <chico.carolina@gmail.com>
2020-07-23 17:35:31 -0300

    Modulo 001 - Ejercicio 4.5 Control de Flujos For

--- bootcampgl2020/src/main/java/com/globallogic/app/ControlFlujoFor.java
+++ bootcampgl2020/src/main/java/com/globallogic/app/ControlFlujoFor.java
@@ -4,12 +4,24 @@ public class ControlFlujoFor {
 
 	public static void main(String[] args) {
 
-
-		for (int i = 1; i <= 10; i++) {
+	/*	for (int i = 1; i <= 10; i++) {
 			
 			System.out.println("Valor iterado: " + i);
 		}
-
+*/
+		int contador = 0;
+		
+		
+		for (int i = 0; i <= 10000; i++) {
+			
+			if (i % 20 == 0) {
+				
+				contador++;
+				
+			}
+		}
+		
+		System.out.println("Valor del contador: " + contador);
 	}
 
 }
