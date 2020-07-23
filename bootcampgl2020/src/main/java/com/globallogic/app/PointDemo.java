462fdb160699ac0b5b592e31bced04e76db07f0d
Carolina Chico <chico.carolina@gmail.com>
2020-07-23 09:45:46 -0300

    Modulo 001 - Ejercicio 3.5 Estructuras basicas

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/PointDemo.java
@@ -0,0 +1,16 @@
+package com.globallogic.app;
+
+public class PointDemo {
+	
+	public static void main(String[] args) {
+		
+		VariableInstancia vl = new VariableInstancia();
+		
+		vl.engPoints = 50;
+		vl.mathsPoints = 80;
+
+		System.out.println("Los valores son: " + vl.engPoints + " " + vl.mathsPoints);
+		
+
+}
+}
---
+++ bootcampgl2020/src/main/java/com/globallogic/app/VariableInstancia.java
@@ -0,0 +1,8 @@
+package com.globallogic.app;
+
+public class VariableInstancia {
+	int engPoints;
+	int mathsPoints;
+	int phyPoints;
+	
+}
