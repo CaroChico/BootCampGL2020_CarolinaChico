62defc016a7e1ca08ba2cfc48df6f7771a366997
Carolina Chico <chico.carolina@gmail.com>
2020-07-23 17:14:26 -0300

    Modulo 001 - Ejercicio 4.4 Control de Flujos For

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/ControlFlujoFor.java
@@ -0,0 +1,15 @@
+package com.globallogic.app;
+
+public class ControlFlujoFor {
+
+	public static void main(String[] args) {
+
+
+		for (int i = 1; i <= 10; i++) {
+			
+			System.out.println("Valor iterado: " + i);
+		}
+
+	}
+
+}
