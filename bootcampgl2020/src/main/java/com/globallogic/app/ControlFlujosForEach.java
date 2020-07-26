3fbdb9b3501ef28a6bc0e670f982c17b37f71087
Carolina Chico <chico.carolina@gmail.com>
2020-07-26 17:44:53 -0300

    Modulo 001 - Ejercicio 4.9 Control Flujos For Each

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/ControlFlujosForEach.java
@@ -0,0 +1,17 @@
+package com.globallogic.app;
+
+public class ControlFlujosForEach {
+
+	public static void main(String[] args) {
+
+		int[] num = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		 		
+		for (int i = 0; i < 10; i++) {
+			
+		System.out.println("Valor iterado: " + num [i]);
+		}
+
+	}
+
+}
