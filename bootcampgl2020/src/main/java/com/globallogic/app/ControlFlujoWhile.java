7966a35e932d4224ef370149a43f51b590fd5d6c
Carolina Chico <chico.carolina@gmail.com>
2020-07-26 17:24:33 -0300

    Modulo 001 - Ejercicio 4.8 Control de flujos While

--- bootcampgl2020/src/main/java/com/globallogic/app/ControlFlujoWhile.java
+++ bootcampgl2020/src/main/java/com/globallogic/app/ControlFlujoWhile.java
@@ -2,17 +2,29 @@ package com.globallogic.app;
 
 public class ControlFlujoWhile {
 
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
+
+		/*
+		 * int i = 1;
+		 * 
+		 * while (i<=10) { System.out.println("Numero iterado: " + i); i++;
+		 * 
+		 * }
+		 */
+
 		int i = 1;
-		
-		while (i<=10) {
-			System.out.println("Numero iterado: " + i);
+
+		while (i <= 50) {
+
+			if (i % 2 == 0) {
+
+				System.out.println("Valor iterado: " + i + " es par");
+			} else {
+
+				System.out.println("Valor iterado: " + i + " es impar");
+			}
 			i++;
-			
 		}
-						
+
 	}
-	
-	
 }
