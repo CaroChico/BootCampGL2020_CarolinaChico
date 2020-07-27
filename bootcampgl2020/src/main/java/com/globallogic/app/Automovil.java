277daf70fa363b317755f74473edcc6e785013df
Carolina Chico <chico.carolina@gmail.com>
2020-07-27 10:30:42 -0300

    Modulo 001 - Ejercicio 5.4 Abstraccion

---
+++ bootcampgl2020/src/main/java/com/globallogic/app/Automovil.java
@@ -0,0 +1,12 @@
+package com.globallogic.app;
+
+abstract class Automovil {
+
+		void avanzar() {
+		System.out.println("Soy el metodo avanzar de Automovil");
+	}
+	
+	
+	
+	
+}
---
+++ bootcampgl2020/src/main/java/com/globallogic/app/Berlina.java
@@ -0,0 +1,19 @@
+package com.globallogic.app;
+
+public class Berlina extends Automovil {
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Automovil b1 = new Berlina();
+	
+	b1.avanzar();
+	
+	
+		
+	}
+
+}
